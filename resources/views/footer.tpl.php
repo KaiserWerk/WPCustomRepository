528278d314aac05c8eb5d0c29001244432227df4
Robin Kaiser <web@r-k.mx>
2018-05-23 14:40:18 +0200

    removed the copyright row in footer

--- resources/views/footer.tpl.php
+++ resources/views/footer.tpl.php
@@ -1,9 +1,4 @@
-    <hr>
-    
-    <footer>
-        <p>&copy; Company 2015</p>
-    </footer>
-</div> <!-- /container -->
+
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 
