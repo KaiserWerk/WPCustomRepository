bb780c56cbb4d556ac07fa95c9653cff08ef72e0
Robin Kaiser <web@r-k.mx>
2018-05-23 17:06:12 +0200

    added a copyright notice placeholder in footer

--- resources/views/footer.tpl.php
+++ resources/views/footer.tpl.php
@@ -1,4 +1,11 @@
-
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <hr>
+            &copy; 2016
+        </div>
+    </div>
+</div>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="/assets/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 
