f467937ff9f8e8d55e1d9856237a65f62687854f
Robin Kaiser <web@r-k.mx>
2018-05-23 17:05:17 +0200

    renamed and visually upgraded the 404 error page template

--- resources/views/error/404.tpl.php
+++ resources/views/error/404.tpl.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <title>Page Not Found</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/bootstrap-theme.min.css">
     <style>
 
         * {
@@ -55,6 +57,10 @@
 <body>
     <h1>Page Not Found</h1>
     <p>Sorry, but the page you were trying to view does not exist.</p>
+    <br>
+    <a href="/" class="btn btn-primary btn-lg">
+        <span class="glyphicon glyphicon-home"></span> Take Me Home
+    </a>
 </body>
 </html>
 <!-- IE needs 512+ bytes: http://blogs.msdn.com/b/ieinternals/archive/2010/08/19/http-error-pages-in-internet-explorer.aspx -->
