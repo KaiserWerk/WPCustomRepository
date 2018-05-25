d349d0a6eae0ab8540b9dfabab64eb496c7b53e1
Robin Kaiser <web@r-k.mx>
2018-05-25 16:08:59 +0200

    some visual candy

--- resources/views/plugin/list.tpl.php
+++ resources/views/plugin/list.tpl.php
@@ -3,7 +3,7 @@
         <div class="col-md-12">
             <h1 class="mt-5">Plugin list</h1>
             <br>
-            <table class="table">
+            <table class="table table-condense table-bordered table-hover">
                 <thead>
                 <tr>
                     <th>Plugin Name</th>
@@ -19,7 +19,7 @@
                     echo '<td>' . $row['plugin_name'] . '</td>';
                     echo '<td>' . $row['slug'] . '</td>';
                     echo '<td>' . $row['version'] . '</td>';
-                    echo '<td><a href="/plugin/' . $row['id'] . '/edit">Edit</a> <a href="/plugin/' . $row['id'] . '/archive">Archive</a></td>';
+                    echo '<td><a href="/plugin/' . $row['id'] . '/edit">Edit</a> / <a href="/plugin/' . $row['id'] . '/archive">Archive</a></td>';
                     echo '</tr>';
                 }
                 ?>
