9a1af7c9dce5747806785fad7e93c52d74d53ba0
Robin Kaiser <web@r-k.mx>
2018-05-25 13:23:34 +0200

    reworked the entire form, added ckeditor to section_description textarea

--- resources/views/plugin/add.tpl.php
+++ resources/views/plugin/add.tpl.php
@@ -4,31 +4,50 @@
             <h1 class="mt-5">Add Plugin</h1>
             <br>
             <div class="row">
-                <div class="col-md-6">
-                    <form method="post" enctype="multipart/form-data">
-                        <?php AuthHelper::generateCSRFInput(); ?>
-                        Plugin Name:
-                        <input type="text" name="_plugin_add[plugin_name]" class="form-control" required><br>
-                        Plugin Slug:
-                        <input type="text" name="_plugin_add[slug]" class="form-control" required><br>
-                        Version:
-                        <input type="text" name="_plugin_add[version]" class="form-control" required><br>
-                        Requires:
-                        <input type="text" name="_plugin_add[requires]" class="form-control"><br>
-                        Tested:
-                        <input type="text" name="_plugin_add[tested]" class="form-control"><br>
-                        Homepage:
-                        <input type="text" name="_plugin_add[homepage]" class="form-control"><br>
-                        Section Description:
-                        <textarea name="_plugin_add[section_description]" class="form-control" rows="6" required></textarea><br>
-                        File:
-                        <input type="file" name="_plugin_add_plugin_file" class="form-control-file" required><br>
-                        <input type="submit" value="Add Plugin" class="btn btn-primary">
-                    </form>
-                </div>
-                <div class="col-md-6">
-                
-                </div>
+                <form method="post" class="row" enctype="multipart/form-data" style="width: 100%;">
+                    <?php AuthHelper::generateCSRFInput(); ?>
+                    <div class="col-md-6 col-sm-12">
+                        <div class="form-group">
+                            <label for="plugin_name">Plugin Name</label>
+                            <input type="text" name="_plugin_add[plugin_name]" id="plugin_name" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="slug">Plugin Slug</label>
+                            <input type="text" name="_plugin_add[slug]" id="slug" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="version">Version <small>(<a href="https://semver.org/" target="_blank">see SemVer</a>)</small></label>
+                            <input type="text" name="_plugin_add[version]" id="version" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="requires">Requires</label>
+                            <input type="text" name="_plugin_add[requires]" id="requires" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="tested">Tested</label>
+                            <input type="text" name="_plugin_add[tested]" id="tested" class="form-control" value="<?php Helper::getCurrentWPVersion(); ?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="homepage">Homepage</label>
+                            <input type="text" name="_plugin_add[homepage]" id="homepage" class="form-control" placeholder="http://">
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-sm-12">
+                        <div class="form-group">
+                            <label for="section_description">Section Description</label>
+                            <script src="/assets/ckeditor/ckeditor.js"></script>
+                            <textarea name="_plugin_add[section_description]" id="section_description" class="form-control" rows="8" required></textarea>
+                            <script>
+                                CKEDITOR.replace( 'section_description' );
+                            </script>
+                        </div>
+                        <div class="form-group">
+                            <label for="plugin_file">Plugin File</label>
+                            <input type="file" name="_plugin_add_plugin_file" id="plugin_file" class="form-control-file" required>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Add Plugin</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
