b5d3c92e295255e96fd1ec957d4d5a6925aed58b
Robin Kaiser <m@r-k.mx>
2018-05-26 12:03:37 +0200

    some code improvements, added text

--- resources/views/license/add.tpl.php
+++ resources/views/license/add.tpl.php
@@ -44,11 +44,11 @@ $errors = array(
                 AuthHelper::generateHoneypotInput();
                 ?>
                 <div class="form-group">
-                    <label for="license_user">License User</label>
+                    <label for="license_user">License User*</label>
                     <input type="text" name="_add[license_user]" id="license_user" class="form-control" required>
                 </div>
                 <div class="form-group">
-                    <label for="license_key">License Key</label>
+                    <label for="license_key">License Key*</label>
                     <textarea name="_add[license_key]" id="license_key" class="form-control" maxlength="200" rows="4" required><?php echo $key; ?></textarea>
                 </div>
                 <div class="form-group">
@@ -56,10 +56,10 @@ $errors = array(
                     <input type="text" name="_add[license_host]" id="license_host" class="form-control">
                 </div>
                 <div class="form-group">
-                    <label for="plugin_slug">Plugin</label>
+                    <label for="plugin_slug">Plugin*</label>
                     <select name="_add[plugin_slug]" id="plugin_slug" size="1" class="form-control">
                         <?php
-                       
+                        echo '<option value=""><i>None</i></option>';
                         foreach ($pluginSlugSelections as $pluginSlugSelection) {
                             echo '<option value="';
                             echo $pluginSlugSelection['slug'];
@@ -71,7 +71,7 @@ $errors = array(
                     </select>
                 </div>
                 <div class="form-group">
-                    <label for="valid_until">Valid Until</label>
+                    <label for="valid_until">Valid Until*</label>
                     <input type="text" name="_add[valid_until]" id="valid_until" class="form-control" required value="<?php echo date('Y')+1 . '-' . date('m-d'); ?>">
                 </div>
                 <div class="form-group">
@@ -79,6 +79,16 @@ $errors = array(
                     <button class="btn btn-success" name="license_button">Add New License</button>
                 </div>
             </form>
+        </div>
+        <div class="col-md-6">
+            <h1 class="mt-5">Info</h1>
+            <br>
+            <ul>
+                <li>License keys can be up to 200 characters long, but do not have to be. They can be shorter.</li>
+                <li>By entering a host, you can restrict the license to this host. This is optional.</li>
+                <li>You can choose a plugin for which the license will only be valid. If you choose none, the license will be valid for all plugins.</li>
+                <li>You can set the validity date as far into the future as you want, but usually, you set it to 1 year (default) and renew the license regularly.</li>
+            </ul>
         </div>
     </div>
 </div>
