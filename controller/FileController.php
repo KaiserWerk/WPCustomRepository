fea31fdd062e3b4e6c592af1ab1f5d272df83fdc
Robin Kaiser <web@r-k.mx>
2018-05-29 16:38:44 +0200

    improved some queries, added a failure case

--- controller/FileController.php
+++ controller/FileController.php
@@ -6,28 +6,32 @@ $klein->respond('GET', '/download/plugin/[:slug]', function ($request) {
     // check license validity
     LicenseHelper::checkLicenseValidity($request);
     
-    $slug = $request->slug;
-    $db = new DBHelper();
-    $row = $db->get('plugin', [
-        'id',
-        'version',
-        'downloaded',
-    ], [
-        'slug' => $slug,
-        'ORDER' => [
-            'version' => 'DESC'
-        ]
-    ]);
+    $slug = $request->slug ?? null;
+    if ($slug !== null) {
+        $db = new DBHelper();
+        $row = $db->get('plugin', [
+            'id',
+            'version',
+        ], [
+            'slug' => $slug,
+            'ORDER' => [
+                'version' => 'DESC'
+            ],
+            'LIMIT' => 1,
+        ]);
     
-    $db->update('plugin', [
-        'downloaded' => $row['downloaded'] + 1,
-    ]);
+        $db->update('plugin', [
+            'downloaded[+]' => 1,
+        ]);
     
-    $file_name = $slug . '_v' . $row['version'] . '.zip';
-    $dir = downloadDir() . '/' . $slug . '/';
+        $file_name = $slug . '_v' . $row['version'] . '.zip';
+        $dir = downloadDir() . '/' . $slug . '/';
     
-    header('Content-Type: application/zip');
-    header('Content-Disposition: attachment; filename="'.$file_name.'"');
-    readfile($dir . $file_name);
-    die;
+        header('Content-Type: application/zip');
+        header('Content-Disposition: attachment; filename="' . $file_name . '"');
+        readfile($dir . $file_name);
+        die;
+    } else {
+        echo 'invalid plugin slug';
+    }
 });
