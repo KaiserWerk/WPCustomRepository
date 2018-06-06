cfee163138b2fb47f07583843104ff98b9bc9a8b
Robin Kaiser <web@r-k.mx>
2018-06-06 17:25:26 +0200

    added ThemeAPIController

---
+++ controller/ThemeAPIController.php
@@ -0,0 +1,71 @@
+<?php
+
+$klein->respond('GET', '/api/themes/theme-update/[:slug]', function($request) {
+    $slug = $request->slug ?? null;
+    
+    if ($slug === null) {
+        die('invalid slug');
+    }
+    
+    LicenseHelper::checkLicenseValidity($request->headers);
+    
+    $db = new DBHelper();
+    
+    $latest_version = $db->get('theme_version', '*', [
+        'slug' => $slug,
+        'ORDER' => [
+            'version' => 'DESC',
+        ],
+    ]);
+    
+    $response = array();
+    $response['package'] = Helper::getHost() . '/download/theme/' . $slug . '/latest';
+    $response['new_version'] = $latest_version['version'];
+    $response['url'] = $latest_version['url'];
+    $response['tags'] = array(
+        'one' => 'ins',
+        'two' => 'tzwi',
+    );
+    
+    echo json_encode($response);
+    
+});
+
+$klein->respond('GET', '/api/themes/theme-information/[:slug]', function($request) {
+    $slug = $request->slug ?? null;
+    
+    if ($slug === null) {
+        die('invalid slug');
+    }
+    
+    LicenseHelper::checkLicenseValidity($request->headers);
+    
+    $db = new DBHelper();
+    
+    $base_theme = $db->get('theme', '*', [
+        'slug' => $slug,
+    ]);
+    
+    $latest_version = $db->get('theme_version', '*', [
+        'slug' => $slug,
+        'ORDER' => [
+            'version' => 'DESC',
+        ],
+    ]);
+    
+    $data = new stdClass;
+    $data->slug = $slug;
+    $data->name = $base_theme['theme_name'];
+    $data->version = $latest_version['version'];
+    $data->last_updated = $base_theme['updated_at'];
+    $data->download_link = Helper::getHost() . '/download/theme/' . $slug . '/latest';
+    $data->author = $base_theme['author'];
+    $data->requires = $latest_version['requires'];
+    $data->tested = $latest_version['requires'];
+    $data->screenshot_url = 'https://www.whatsbroadcast.com/wp-content/uploads/2017/09/API.png'; // TODO
+    $data->sections = array(
+        'description' => $base_theme['section_description'],
+    );
+    echo json_encode($data);
+    
+});
