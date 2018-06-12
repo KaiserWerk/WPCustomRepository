7345c9589ccf0330c489e755bd0f8722b7c39460
Robin Kaiser <m@r-k.mx>
2018-06-13 00:42:05 +0200

    added ThemeController with empty routes

---
+++ controller/ThemeController.php
@@ -0,0 +1,29 @@
+<?php
+
+$klein->respond('GET', '/theme/list', function () {
+
+});
+
+$klein->respond('GET', '/theme/base/add', function () {
+
+});
+
+$klein->respond('GET', '/theme/base/edit', function () {
+
+});
+
+$klein->respond('GET', '/theme/base/remove', function () {
+
+});
+
+$klein->respond('GET', '/theme/version/add', function () {
+
+});
+
+$klein->respond('GET', '/theme/version/edit', function () {
+
+});
+
+$klein->respond('GET', '/theme/version/remove', function () {
+
+});
