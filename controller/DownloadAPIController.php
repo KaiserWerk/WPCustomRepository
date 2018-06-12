967c6d940652a11da30ad5a51adeec32b68b19e0
Robin Kaiser <web@r-k.mx>
2018-06-12 15:11:47 +0200

    renamed FileController to DownloadAPIController

--- controller/DownloadAPIController.php
+++ controller/DownloadAPIController.php
@@ -56,3 +56,60 @@ $klein->respond('GET', '/download/plugin/[:slug]/[:version]', function ($request
         die('invalid plugin slug or version constraint');
     }
 });
+
+// returns the most recent theme version
+$klein->respond('GET', '/download/theme/[:slug]/[:version]', function ($request) {
+    
+    if (!AuthHelper::isLoggedIn()) {
+        LicenseHelper::checkLicenseValidity($request->headers);
+    }
+    
+    $slug = $request->slug ?? null;
+    $version = $request->version ?? null;
+    
+    $db = new DBHelper();
+    
+    $base_theme = $db->get('theme', [
+        'id',
+    ], [
+        'slug' => $slug,
+    ]);
+    
+    $latest_version = $db->max('theme_version', 'version', [
+        'theme_entry_id' => $base_theme['id'],
+    ]);
+    
+    if ($latest_version === false) {
+        die('No files available');
+    }
+    
+    if ($slug !== null && $version !== null) {
+        
+        if ($version === 'latest') {
+            $download_version = $latest_version;
+        } else {
+            $download_version = $version;
+        }
+        
+        /*$db->update('theme_version', [
+            'downloaded[+]' => 1,
+        ], [
+            'theme_entry_id' => $base_theme['id'],
+            'version' => $download_version,
+        ]);*/
+        
+        $file_name = $slug . '_v' . $download_version . '.zip';
+        $dir = downloadDir() . '/' . $slug . '/';
+        
+        if (file_exists($dir . $file_name)) {
+            header('Content-Type: application/zip');
+            header('Content-Disposition: attachment; filename="' . $file_name . '"');
+            readfile($dir . $file_name);
+            die;
+        } else {
+            die('Version file does not exist');
+        }
+    } else {
+        die('invalid theme slug or version constraint');
+    }
+});
