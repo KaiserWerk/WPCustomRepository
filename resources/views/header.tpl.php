801fb3f00e719a0f08270d4640887b731c9533aa
Robin Kaiser <web@r-k.mx>
2018-06-19 15:28:10 +0200

    changed the bootstrap <link>

--- resources/views/header.tpl.php
+++ resources/views/header.tpl.php
@@ -11,7 +11,7 @@ $StopwatchHelper->start();
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/bootstrap/css/bootstrap-united.min.css">
     <link rel="stylesheet" href="/assets/font-awesome/css/font-awesome.min.css">
     <style>
         body {
