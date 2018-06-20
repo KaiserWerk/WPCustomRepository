2d2d93b694aeed44c732233a72cee899ef6edfba
Robin Kaiser <web@r-k.mx>
2018-06-20 18:02:37 +0200

    added functionality to /theme/version/list, added some basic constructs

--- controller/ThemeVersionController.php
+++ controller/ThemeVersionController.php
@@ -7,6 +7,40 @@ $router->with('/theme/version', function () use ($router) {
      */
     $router->respond('GET', '/[:id]/list', function ($request) {
         AuthHelper::requireLogin();
+        
+        $id = $request->id;
+        
+        $db = new DBHelper();
+        
+        $base_theme = $db->get('theme', '*', [
+            'id' => $id,
+        ]);
+        
+        if ($base_theme === false) {
+            Helper::setMessage('Base theme not found!', 'danger');
+            Helper::redirect('/theme/base/list');
+        }
+        
+        $theme_versions = $db->select('theme_version', '*', [
+            'theme_entry_id' => $id,
+        ]);
+    
+        if ($theme_versions === false) {
+            Helper::setMessage('No theme versions found!', 'danger');
+            Helper::redirect('/theme/base/list');
+        }
+        
+        Helper::renderPage('/theme/list_version.tpl.php', [
+            'base_theme' => $base_theme,
+            'theme_versions' => $theme_versions,
+        ]);
+    });
+    
+    /**
+     * Show details of a theme version
+     */
+    $router->respond('GET', '/[:id]/show', function ($request) {
+        AuthHelper::requireLogin();
     });
     
     /**
@@ -14,6 +48,23 @@ $router->with('/theme/version', function () use ($router) {
      */
     $router->respond('GET', '/add', function ($request) {
         AuthHelper::requireLogin();
+        $db = new DBHelper();
+        if (isset($_POST['btn_theme_version_add'])) {
+        
+        } else {
+            $base_themes = $db->select('theme', [
+                'id',
+                'theme_name',
+            ], [
+                'ORDER' => [
+                    'theme_name' => 'DESC',
+                ]
+            ]);
+            
+            Helper::renderPage('/theme/add_version.tpl.php', [
+                'base_themes' => $base_themes,
+            ]);
+        }
     });
     
     /**
@@ -21,6 +72,23 @@ $router->with('/theme/version', function () use ($router) {
      */
     $router->respond('GET', '/[:id]/edit', function ($request) {
         AuthHelper::requireLogin();
+        $db = new DBHelper();
+        if (isset($_POST['btn_theme_version_edit'])) {
+        
+        } else {
+            $base_themes = $db->select('theme', [
+                'id',
+                'theme_name',
+            ], [
+                'ORDER' => [
+                    'theme_name' => 'DESC',
+                ]
+            ]);
+        
+            Helper::renderPage('/theme/edit_version.tpl.php', [
+                'base_themes' => $base_themes,
+            ]);
+        }
     });
     
     /**
@@ -30,10 +98,4 @@ $router->with('/theme/version', function () use ($router) {
         AuthHelper::requireLogin();
     });
     
-    /**
-     * Show details of a theme version
-     */
-    $router->respond('GET', '/[:id]/show', function ($request) {
-        AuthHelper::requireLogin();
-    });
 });
