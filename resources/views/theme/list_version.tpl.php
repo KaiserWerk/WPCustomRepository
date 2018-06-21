cbbfbdab7d98b11f00d886364646bbb47b58cd93
Robin Kaiser <web@r-k.mx>
2018-06-21 15:45:22 +0200

    used shorthand syntax in view

--- resources/views/theme/list_version.tpl.php
+++ resources/views/theme/list_version.tpl.php
@@ -10,12 +10,12 @@
                     <?php foreach ($theme_versions as $entry): ?>
                     <tr>
                         <td class="text-right"><small><b>v<?=$entry['version'];?></b>
-                        (<a href="/download/plugin/<?=$base_theme['slug'];?>/<?=$entry['version'];?>"><i class="fa fa-download"></i></a>) </small></td>
+                        (<a href="/download/theme/<?=$base_theme['slug'];?>/<?=$entry['version'];?>"><i class="fa fa-download"></i></a>) </small></td>
                         <td><small>Requires WP: <?=$entry['requires'];?></small></td>
                         <td><small>Tested: <?=$entry['tested'];?></small></td>
                         <td><small>Requires PHP: <?=$entry['requires_php'];?></small></td>
                         <td><small>Downloaded: <?=$entry['downloaded'];?></small></td>
-                        <td><small>Installations: <?=$entry['active_installations'];?></small></td>';
+                        <td><small>Installations: <?=$entry['active_installations'];?></small></td>
                         <td><small>Added: <?=(new \DateTime($entry['added_at']))->format('Y-m-d');?></small></td>
                         <td><small>
                         <a href="/theme/version/<?=$entry['id'];?>/show">Show</a>
