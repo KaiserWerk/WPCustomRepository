1c963a0239cf4e80766353a54699ff58fbf45cab
Robin Kaiser <web@r-k.mx>
2018-07-03 17:55:51 +0200

    improved the LicenseHelper

--- helper/LicenseHelper.php
+++ helper/LicenseHelper.php
@@ -13,6 +13,7 @@ class LicenseHelper
         $headers = getallheaders();
         $licenseUser = $headers['X-License-User'] ?? null;
         $licenseKey  = $headers['X-License-Key'] ?? null;
+        $licenseObject  = $headers['X-License-Object'] ?? null;
         $licenseHost = Helper::getIP();
     
         if ($licenseUser === null || $licenseKey === null) {
@@ -26,35 +27,73 @@ class LicenseHelper
         $license = $db->get('license', '*', [
             'license_key' => $licenseKey,
         ]);
-        if ($license !== false) {
-            if ($license['license_user'] !== $licenseUser) {
-                HTTPHelper::jsonResponse([
-                    'status' => 'error',
-                    'message' => 'Invalid license user',
-                ], 401);
-            }
-            
-            if (!empty($license['license_host']) && $license['license_host'] !== $licenseHost) {
-                HTTPHelper::jsonResponse([
-                    'status' => 'error',
-                    'message' => 'Invalid host',
-                ], 401);
-            }
-            
-            $valid = new \DateTime($license['valid_until']);
-            $now = new \DateTime();
-            if ($valid <= $now) {
-                HTTPHelper::jsonResponse([
-                    'status' => 'error',
-                    'message' => 'License expired',
-                ], 410);
-            }
-        } else {
+        if ($license === false) {
             HTTPHelper::jsonResponse([
                 'status' => 'error',
                 'message' => 'License not found',
             ], 404);
         }
-        return true;
+        if ($license['license_user'] !== $licenseUser) {
+            HTTPHelper::jsonResponse([
+                'status' => 'error',
+                'message' => 'Invalid license user',
+            ], 401);
+        }
+        
+        if (!empty($license['license_host']) && $license['license_host'] !== $licenseHost) {
+            HTTPHelper::jsonResponse([
+                'status' => 'error',
+                'message' => 'Invalid host',
+            ], 401);
+        }
+        
+        $valid = new \DateTime($license['valid_until']);
+        $now = new \DateTime();
+        if ($valid <= $now) {
+            HTTPHelper::jsonResponse([
+                'status' => 'error',
+                'message' => 'License expired',
+            ], 410);
+        }
+        
+        if ($license['plugin_entry_id'] === null && $license['theme_entry_id'] === null) {
+            HTTPHelper::jsonResponse([
+                'status' => 'error',
+                'message' => 'License not valid for any object',
+            ], 400);
+        }
+        
+        if ($license['plugin_entry_id'] !== null || $license['theme_entry_id'] !== null) {
+            $objectParts = explode('_', $licenseObject);
+            // 0 = pluginversion|themeversion
+            // 1 = slug
+            if ($objectParts[0] === 'pluginversion') {
+                
+                $base_plugin = $db->get('plugin', 'id', [
+                    'slug' => $objectParts[1],
+                ]);
+                
+                if ($base_plugin['id'] !== $license['plugin_entry_id']) {
+                    HTTPHelper::jsonResponse([
+                        'status' => 'error',
+                        'message' => 'License not valid for object (1)',
+                    ], 400);
+                }
+            } elseif ($objectParts[0] === 'themeversion') {
+
+                $base_theme = $db->get('theme', 'id', [
+                    'slug' => $objectParts[1],
+                ]);
+                
+                if ($base_theme['id'] !== $license['theme_entry_id']) {
+                    HTTPHelper::jsonResponse([
+                        'status' => 'error',
+                        'message' => 'License not valid for object (2)',
+                    ], 400);
+                }
+            }
+        }
+        
+        return true; // a formality
     }
 }
