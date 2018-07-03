bd735b07c34ac7e4ed97fb1ff6a0b5cb5fa7f178
Robin Kaiser <web@r-k.mx>
2018-07-03 17:55:15 +0200

    added informationController

---
+++ controller/InformationController.php
@@ -0,0 +1,32 @@
+<?php
+AuthHelper::init();
+$router->with('/api/information', function () use ($router) {
+    
+    $router->respond('GET', '/license', function ($request) {
+        $headers = getallheaders();
+        $licenseKey  = $headers['X-License-Key'] ?? null;
+        
+        if ($licenseKey === null) {
+            HTTPHelper::jsonResponse([
+                'status' => 'error',
+                'message' => 'Missing license data',
+            ], 400);
+        }
+        
+        $db = new DBHelper();
+        $license = $db->get('license', '*', [
+            'license_key' => $licenseKey
+        ]);
+        
+        HTTPHelper::jsonResponse([
+            'status' => 'success',
+            'message' => '',
+            'body' => [
+                'valid_until' => $license['valid_until'],
+            ]
+        ]);
+        
+    });
+    
+    
+});
