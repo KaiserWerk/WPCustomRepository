66683fbc2c8b22bd6f42ba21b452a437eaa11d34
Robin Kaiser <web@r-k.mx>
2018-07-09 16:54:34 +0200

    updated DBHelper for SQLite support

--- helper/DBHelper.php
+++ helper/DBHelper.php
@@ -6,17 +6,23 @@ class DBHelper extends Medoo
 {
     public function __construct()
     {
-        parent::__construct([
-            'database_type' => getenv('DB_DRIVER'),
-            'server' => getenv('DB_HOST'),
-            'database_name' => getenv('DB_NAME'),
-            'username' => getenv('DB_USER'),
-            'password' => getenv('DB_PASSWORD'),
-            'prefix' => getenv('DB_PREFIX'),
-            'port' => getenv('DB_PORT'),
-            'charset' => getenv('DB_CHARSET'),
-            'database_file' => getenv('DB_FILE')
-        ]);
+        if (getenv('DB_DRIVER') === 'mysql') {
+            parent::__construct([
+                'database_type' => getenv('DB_DRIVER'),
+                'server' => getenv('DB_HOST'),
+                'database_name' => getenv('DB_NAME'),
+                'username' => getenv('DB_USER'),
+                'password' => getenv('DB_PASSWORD'),
+                'prefix' => getenv('DB_PREFIX'),
+                'port' => getenv('DB_PORT'),
+                'charset' => getenv('DB_CHARSET'),
+            ]);
+        } elseif (getenv('DB_DRIVER') === 'sqlite') {
+            parent::__construct([
+                'database_type' => getenv('DB_DRIVER'),
+                'database_file' => getenv('DB_FILE'),
+            ]);
+        }
     }
 
     public function hasError()
