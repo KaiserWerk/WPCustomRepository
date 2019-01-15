8d06a58671197f03a379cf25abe4ba38b9b5dbcf
Robin Kaiser <m@r-k.mx>
2019-01-15 06:14:54 +0100

    renamed MainController to DefaultController

---
+++ controller/DefaultController.php
@@ -0,0 +1,5 @@
+<?php
+$router->respond( 'GET', '/', function ( $request ) {
+    AuthHelper::init();
+    Helper::renderPage( '/index.tpl.php' );
+} );
--- controller/MainController.php
+++
@@ -1,5 +0,0 @@
-<?php
-$router->respond('GET', '/', function ($request) {
-    AuthHelper::init();
-    Helper::renderPage('/index.tpl.php');
-});
