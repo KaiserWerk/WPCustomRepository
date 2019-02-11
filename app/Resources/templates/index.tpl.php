df42cf06140840ffc19338b83b23a0a9c28d07d8
Robin Kaiser <m@r-k.mx>
2019-02-11 08:41:02 +0100

    Update index.tpl.php
    changed intro text

--- app/Resources/templates/index.tpl.php
+++ app/Resources/templates/index.tpl.php
@@ -5,8 +5,7 @@
             <h1 class="mt-5">WPCustomRepository</h1>
             <?php Helper::getMessage(); ?>
             <br>
-            WPCR offers the amazing possibility of automatic updates for your custom plugins (and maybe soon themes as
-            well).
+            WPCR offers the amazing possibility of automatic updates for your custom plugins and themes.
         </div>
     </div>
 </div>
